650c30d4727422131ef8a57e66986098ae42071c
Dmitri Bogomolov <4glitch@gmail.com>
2018-02-26 12:36:00 +0200

    fromAddress - str, subject - unicode
    (for simple encoding like for extended)

--- src/bitmessageqt/uisignaler.py
+++ src/bitmessageqt/uisignaler.py
@@ -12,8 +12,9 @@ class UISignaler(QtCore.QThread):
     updateStatusBar = QtCore.Signal(object)
     updateSentItemStatusByToAddress = QtCore.Signal(object, str)
     updateSentItemStatusByAckdata = QtCore.Signal(object, str)
-    displayNewInboxMessage = QtCore.Signal(object, str, str, object, str)
-    displayNewSentMessage = QtCore.Signal(object, str, str, str, object, str)
+    displayNewInboxMessage = QtCore.Signal(object, str, object, object, str)
+    displayNewSentMessage = QtCore.Signal(
+        object, str, str, object, object, str)
     updateNetworkStatusTab = QtCore.Signal(bool, bool, Peer)
     updateNumberOfMessagesProcessed = QtCore.Signal()
     updateNumberOfPubkeysProcessed = QtCore.Signal()
@@ -52,9 +53,10 @@ class UISignaler(QtCore.QThread):
                 self.updateSentItemStatusByAckdata.emit(ackData, message)
             elif command == 'displayNewInboxMessage':
                 inventoryHash, toAddress, fromAddress, subject, body = data
+
                 self.displayNewInboxMessage.emit(
                     inventoryHash, toAddress, fromAddress,
-                    unicode(subject, 'utf-8'), body)
+                    subject, body)
             elif command == 'displayNewSentMessage':
                 toAddress, fromLabel, fromAddress, subject, message, ackdata = data
                 self.displayNewSentMessage.emit(
