197309308207c48f6ff326e9bb911ca43a989eda
Dmitri Bogomolov <4glitch@gmail.com>
2018-02-26 12:36:00 +0200

    fromAddress - str, subject - unicode   (for simple encoding like for extended)

--- src/helper_msgcoding.py
+++ src/helper_msgcoding.py
@@ -136,8 +136,10 @@ class MsgDecode(object):
         # Throw away any extra lines (headers) after the subject.
         if subject:
             subject = subject.splitlines()[0]
-        self.subject = subject
-        self.body = body
+        # Field types should be the same for all message types
+        self.subject = unicode(subject, 'utf-8', 'replace')
+        self.body = unicode(body, 'utf-8', 'replace')
+
 
 if __name__ == '__main__':
     import random
