2d1c75f4f7d4a64672e15502736b2606e524527b
Dmitri Bogomolov <4glitch@gmail.com>
2021-01-29 19:33:08 +0200

    Format and explicitly skip test_openclpow

--- src/tests/test_openclpow.py
+++ src/tests/test_openclpow.py
@@ -5,12 +5,14 @@ import hashlib
 import unittest
 from struct import pack, unpack
 
-from .test_process import TestProcessProto
+from .common import skip_python3
 
-import pybitmessage.openclpow as openclpow
+skip_python3()  # noqa:E402
+
+from pybitmessage import openclpow
 
 
-class TestOpenClPow(TestProcessProto):
+class TestOpenCLPow(unittest.TestCase):
     """
     Main opencl test case
     """
@@ -18,16 +20,17 @@ class TestOpenClPow(TestProcessProto):
     @classmethod
     def setUpClass(cls):
         openclpow.initCL()
-        super(TestOpenClPow, cls).setUpClass()
 
-    @unittest.skipIf(not openclpow.enabledGpus,
-                     "No GPUs found / enabled")
+    @unittest.skipUnless(openclpow.enabledGpus, "No GPUs found / enabled")
     def test_openclpow(self):
         """Check the working of openclpow module"""
         target_ = 54227212183
-        initialHash = ("3758f55b5a8d902fd3597e4ce6a2d3f23daff735f65d9698c270987f4e67ad590"
-                       "b93f3ffeba0ef2fd08a8dc2f87b68ae5a0dc819ab57f22ad2c4c9c8618a43b3").decode("hex")
+        initialHash = (
+            "3758f55b5a8d902fd3597e4ce6a2d3f23daff735f65d9698c270987f4e67ad590"
+            "b93f3ffeba0ef2fd08a8dc2f87b68ae5a0dc819ab57f22ad2c4c9c8618a43b3"
+        ).decode("hex")
         nonce = openclpow.do_opencl_pow(initialHash.encode("hex"), target_)
         trialValue, = unpack(
-            '>Q', hashlib.sha512(hashlib.sha512(pack('>Q', nonce) + initialHash).digest()).digest()[0:8])
+            '>Q', hashlib.sha512(hashlib.sha512(
+                pack('>Q', nonce) + initialHash).digest()).digest()[0:8])
         self.assertLess((nonce - trialValue), target_)
